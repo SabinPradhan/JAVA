79fd066b03307987630cc3b2e694fed148befa53
SabinPradhan <71056102+SabinPradhan@users.noreply.github.com>
2023-05-21 15:15:25 +0545

    Add files via upload

---
+++ InetDemo2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+public class InetDemo2
+{
+public static void main(String[] arg) throws Exception{
+InetAddress ip=Inet4Address.getByName("www.google.com");
+InetAddress ip1[]=InetAddress.getAllByName("www.facebook.com");
+byte addr[]={20,3,2,12};
+System.out.println("ip:"+ip);
+System.out.print("\nip1:"+ip1);
+InetAddress ip2=InetAddress.getByAddress(addr);
+System.out.print("\nip2 : "+ip2);
+System.out.print("\nAddress :"+ ip.getAddress());
+System.out.print("\nHost Address : "+ip.getHostAddress());
+System.out.print("\nisAnyLocalAddress : "+ip.isAnyLocalAddress());
+System.out.print("\nisLinkLocalAddress : "+ip.isLinkLocalAddress());
+System.out.print("\nisLoopbackAddress : "+ip.isLoopbackAddress());
+System.out.print("\nisMCGlobal : "+ip.isMCGlobal());
+System.out.print("\nisMCLinkLocal : "+ip.isMCLinkLocal());
+System.out.print("\nisMCNodeLocal : "+ip.isMCNodeLocal());
+System.out.print("\nisMCOrgLocal : "+ip.isMCOrgLocal());
+System.out.print("\nisMCSiteLocal : "+ip.isMCSiteLocal());
+System.out.print("\nisMulticastAddress : "+ip.isMulticastAddress());
+System.out.print("\nisSiteLocalAddress : "+ip.isSiteLocalAddress());
+}
+}
